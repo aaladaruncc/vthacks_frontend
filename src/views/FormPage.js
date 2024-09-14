10bf4f718f91338b6a9056dd9c746f8e9146b328
Devin Bhatt <devinbhatt@Devins-MacBook-Air.local>
2024-09-14 16:45:43 -0400

    form

---
+++ src/views/FormPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Form from './components/Form'; // Ensure you import the correct Form component
+
+const FormPage = () => {
+  return (
+    <Form />
+  );
+};
+
+export default FormPage;
